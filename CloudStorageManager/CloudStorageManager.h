5647f82b88898de9dc45a72660a24d3fe2b4d0de
ikawashima41 <i.kawashima41@gmail.com>
2019-12-28 21:58:29 +0900

    Initial Commit

---
+++ CloudStorageManager/CloudStorageManager.h
@@ -0,0 +1,19 @@
+//
+//  CloudStorageManager.h
+//  CloudStorageManager
+//
+//  Created by Iichiro Kawashima on 2019/12/28.
+//  Copyright © 2019 Iichiro Kawashima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CloudStorageManager.
+FOUNDATION_EXPORT double CloudStorageManagerVersionNumber;
+
+//! Project version string for CloudStorageManager.
+FOUNDATION_EXPORT const unsigned char CloudStorageManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudStorageManager/PublicHeader.h>
+
+
